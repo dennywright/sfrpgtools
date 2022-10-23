a4353e44e66082880251aa35a62144ef4ecdab27
Denny Wright <dennyw17@gmail.com>
2022-10-23 10:56:39 -0600

    Add debug logging

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 # run.py
 
+import logging
+
 from app import app
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    app.run(threaded=True)
